c950d8349b08e93b7344b8bfb8348f538e7ada91
Odee <odjhey@gmail.com>
2020-05-31 06:01:38 +0800

    style(theme): add dark mode

---
+++ components/theme-switch.tsx
@@ -0,0 +1,12 @@
+import Switch from "@material-ui/core/Switch";
+
+export default () => {
+  return <div></div>;
+  return (
+    <Switch
+      defaultChecked
+      color="default"
+      inputProps={{ "aria-label": "checkbox with default color" }}
+    />
+  );
+};
