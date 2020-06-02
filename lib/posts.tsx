adaa33ab134d9c908963d27e858071172c187194
Odee <odjhey@gmail.com>
2020-06-03 05:30:54 +0800

    chore(lib): exclude dirs from fileread inside posts/

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -8,7 +8,11 @@ const postsDirectory = path.join(process.cwd(), "posts");
 
 export function getSortedPostsData() {
   // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
+  const fileNames = fs
+    .readdirSync(postsDirectory, { withFileTypes: true })
+    .filter((f) => !f.isDirectory())
+    .map((f) => f.name);
+  console.log(fileNames);
   const allPostsData = fileNames.map((fileName) => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, "");
