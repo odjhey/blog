79f556fc4ac73f88f7e3edbe5ecb3ea05896930b
Odee <odjhey@gmail.com>
2020-06-04 11:09:03 +0800

    chore(gtags): add gtags for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "../styles/global.css";
+import { useEffect } from "react";
+import Router from "next/router";
+import * as gtag from "../lib/gtag";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
